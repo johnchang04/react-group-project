e9baaefe524627c509c60d4914f6701eb1e9d4b6
rachcort <112520070+rachcort@users.noreply.github.com>
2023-10-25 16:19:57 -0700

    Update sizzlingLunch.jsx

--- src/sizzlingLunch.jsx
+++ src/sizzlingLunch.jsx
@@ -8,7 +8,7 @@ function sizzlingLunch() {
     <div>
       <style> {restaurantStyle()} </style>
       <h1> Sizzling Lunch </h1>
-       <img src = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.visitberkeley.com%2Fdirectory%2Fsizzling-lunch%2F&psig=AOvVaw00Sd4XPWLFiOn1uTRsqy2g&ust=1698305312898000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCICOk7nWkIIDFQAAAAAdAAAAABAE'
+       <img src = 'https://www.visitberkeley.com/imager/files_idss_com/C199/ecd694e0-2439-4c8b-b180-e9478ace9153_930104bc5592b0f48aa7a928055610d9.jpg'
             />
       <p className = 'my-element'> Sizzling Lunch on Telegraph Ave is a great place to watch your food cook in front of you.
         They're a great choice to take your parents when they come for the weekend or just for a dinner with friends. 
