1f95497c2bd54b9ce55a8a8d9c8cbc20e3f5bfb5
rachcort <112520070+rachcort@users.noreply.github.com>
2023-10-25 00:34:39 -0700

    Update berkeleyThai.jsx

--- src/berkeleyThai.jsx
+++ src/berkeleyThai.jsx
@@ -1,15 +1,23 @@
 import './App.css'
+import CommentSection from '/comments';
+import restaurantStyle from '.restaurant';
+
 function berkeleyThaiHouse() {
   return (
+    <div> 
+      <style> {restaurantStyle()} </style>
+      <h1> Berkeley Thai House </h1>
       <img src = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.com%2FRestaurant_Review-g32066-d547241-Reviews-Berkeley_thai_House-Berkeley_California.html&psig=AOvVaw1v01lMSUDPBuvn5sbzHgwN&ust=1698294109111000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCJiyy9qskIIDFQAAAAAdAAAAABAE'
             alt = ''
             />
-        <h1> Berkeley Thai House </h1>
-        <p> Berkeley Thai House on Channing Ave is the epitome of middle tier thai food. 
+     <p className="my-element"> Berkeley Thai House on Channing Ave is the epitome of middle tier thai food. 
             You can get a 17 dollar meal that comes with mini appitizers, a plate of noodles or rice, and a drink. 
             It is not an everyday type place, but somewhere you can go with friends to have a dinner outing once and a while. 
             Their patio is nice, when its not freezing in Berkeley. 
             </p>
+    </div>
+      
+       
   )
 }
 export default berkeleyThaiHouse;
